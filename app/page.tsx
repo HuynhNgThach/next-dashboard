ce6d97dc6845f25065380f9837026ee544d2bfbc
thachhn <thach.huynh@codeleap.de>
2024-07-21 19:04:50 +0700

    trigger deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
+'use client'
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 
 import Link from "next/link";
 export default function Page() {
+  function getMapUrl(address = "Ben Thanh, District 1, Ho Chi Minh City, Vietnam") {
+    // URL encode the address
+    const encodedAddress = encodeURIComponent(address);
+
+    // Detect the user's platform
+    const userAgent = navigator.userAgent;
+    let mapUrl = '';
+
+    if (/android/i.test(userAgent)) {
+      // Android platform
+      mapUrl = `geo:0,0?q=${encodedAddress}`;
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      // iOS platform
+      mapUrl = `maps:?q=${encodedAddress}`;
+    } else {
+      // Default to Google Maps web
+      mapUrl = `https://www.google.com/maps/search/?api=1&query=${encodedAddress}`;
+    }
+
+    return mapUrl;
+  }
   return (
     <main className="flex min-h-screen justify-center items-center w-full">
-      <p>new1</p>
+      <a href={getMapUrl()}>Address</a>
       <input type="text" inputMode="numeric" className="h-10" />
     </main>
   );
