43e440f6c3550496dd55701b0f6749230910d227
thachhn <thach.huynh@codeleap.de>
2024-07-21 19:18:35 +0700

    trigger deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 'use client'
-import AcmeLogo from "@/app/ui/acme-logo";
-import { lusitana } from "@/app/ui/fonts";
-import Image from "next/image";
+
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 export default function Page() {
+  const [redered, setRenderd] = useState(false)
+  useEffect(() => {
+    setRenderd(true)
+  }, [])
+  if (!redered) return null
   function getMapUrl(address = "Ben Thanh, District 1, Ho Chi Minh City, Vietnam") {
     if (typeof window !== "undefined") {
       const encodedAddress = encodeURIComponent(address);
 
       // Detect the user's platform
       const userAgent = window.navigator.userAgent;
+      console.log("user agen", userAgent)
       let mapUrl = '';
 
       if (/android/i.test(userAgent)) {
@@ -31,7 +36,7 @@ export default function Page() {
   }
   return (
     <main className="flex min-h-screen justify-center items-center w-full">
-      <a href={getMapUrl()}>Address</a>
+      <a href={getMapUrl()}>address</a>
       <input type="text" inputMode="numeric" className="h-10" />
     </main>
   );
